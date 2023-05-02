7b41caef1e14c15d96a70f0f73c4df90693ec30a
Sankalp12DevOps <sravanthi388@gmail.com>
2023-05-02 20:47:47 +0530

    m

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_lb" "test" {
   name               =  "test-lb-tf-${var.LB_TYPE}"
   internal           =  var.INTERNAL
-  load_balancer_type =  var.LB_TYPE
+  load_balancer_type =  "application"
   security_groups    =  var.INTERNAL ?  aws_security_group.allow_tls_prvt_lb.*.id : aws_security_group.allow_tls_pblc_lb.*.id
   subnets            =  var.INTERNAL ?  data.terraform_remote_state.vpc.outputs.PRVT_SUBNET_IDS : data.terraform_remote_state.vpc.outputs.PUBLC_SUBNET_IDS
   enable_deletion_protection = true
