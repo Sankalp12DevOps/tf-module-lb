31633376bc78f7bea4bb98f135e0cf89f4cd03bc
Sankalp12DevOps <sravanthi388@gmail.com>
2023-05-03 21:03:09 +0530

    m

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "test" {
   load_balancer_type =  "application"
   security_groups    =  var.INTERNAL ?  aws_security_group.allow_tls_prvt_lb.*.id : aws_security_group.allow_tls_pblc_lb.*.id
   subnets            =  var.INTERNAL ?  data.terraform_remote_state.vpc.outputs.PRVT_SUBNET_IDS : data.terraform_remote_state.vpc.outputs.PUBLC_SUBNET_IDS
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   # access_logs {
   #   bucket  = aws_s3_bucket.lb_logs.id
